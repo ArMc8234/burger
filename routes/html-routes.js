368fbcb348ed004122c290aa3de35cfb0b41f690
Aaron McKoy <mckoy_aaron@yahoo.com>
2019-11-27 18:14:06 -0500

    Consoled out handlebars command in html route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,10 +2,8 @@ var db = require("../models");
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        db.Burger.findAll({}).then(function(dbBurger){
-          res.render('index', {
-          burgers: dbBurger
-          });
-        });
+        // db.Burger.findAll({}).then(function(dbBurger){
+            res.render('index');
+        // });
      });
 }
