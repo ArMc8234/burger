d63915cf09da4463bbe63b08559a5a1920ce5f4a
Aaron McKoy <mckoy_aaron@yahoo.com>
2019-11-29 12:54:06 -0500

    Changed console.log reference for app listener.

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ require("./routes/html-routes.js")(app);
 // =============================================================
 db.sequelize.sync({force: true}).then(function(){
     app.listen(PORT, function(){
-        console.log("App listening on: https://localhost:"+ PORT);
+        console.log("App listening on"+ PORT);
     });
 });
