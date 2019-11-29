31f950294bc96d91c292c777043db8acc706d795
Aaron McKoy <mckoy_aaron@yahoo.com>
2019-11-29 10:42:42 -0500

    Changed port reference in app listener

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ require("./routes/html-routes.js")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({force: true}).then(function(){
-    app.listen(PORT, function(){
+    app.listen(process.env.PORT || 3000, function(){
         console.log("App listening on: http://localhost:"+ PORT);
     });
 });
