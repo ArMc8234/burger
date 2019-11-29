35e152344d9604a68a03d7d2c09266830ba38e34
Aaron McKoy <mckoy_aaron@yahoo.com>
2019-11-29 15:37:40 -0500

    Modified index route for test.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        res.render('index');
+        res.render(__dirname,'public/index.html');
     });
 }
        
